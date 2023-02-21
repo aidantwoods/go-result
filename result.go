bae13ab44081ad234a51e387dfa5d672075d7371
Aidan Woods <git@public.aidanwoods.com>
2023-02-21 23:38:36 +0000

    Simplify to one result type, remove reflect, make internal use of option

--- result.go
+++ result.go
@@ -3,158 +3,159 @@ package t
 import "fmt"
 
 type Result[T any] struct {
-	ResultE[T, error]
-}
-
-func (r Result[T]) Results() (*T, error) {
-	r.assertState()
-
-	return r.value, r.UnwrapErrOr(nil)
-}
-
-func (r Result[T]) UnwrappedResults() (T, error) {
-	r.assertState()
-
-	return *r.value, r.UnwrapErrOr(nil)
+	value Option[T]
+	err   Option[error]
 }
 
 func Ok[T any](value T) Result[T] {
 	return Result[T]{
-		ResultE[T, error]{
-			value: &value,
-			err:   nil,
-		},
+		value: Some(value),
+		err:   None[error](),
 	}
 }
 
 func Err[T any](err error) Result[T] {
-	return Result[T]{
-		ResultE[T, error]{
-			value: nil,
-			err:   &err,
-		},
-	}
+	return errGeneric[T](err)
 }
 
-func ErrMap[T any, E error](fn func(error) E) func(e error) Result[T] {
-	return func(e error) Result[T] {
-		var err error
-		err = fn(e)
-		return Result[T]{
-			ResultE[T, error]{
-				value: nil,
-				err:   &err,
-			},
-		}
+func errGeneric[T any, E error](err E) Result[T] {
+	return Result[T]{
+		value: None[T](),
+		err:   Some[error](err),
 	}
 }
 
 func Wrap[T any](format string, err error) Result[T] {
-	wrapped := fmt.Errorf(format, err)
 	return Result[T]{
-		ResultE[T, error]{
-			value: nil,
-			err:   &wrapped,
-		},
+		value: None[T](),
+		err:   Some(fmt.Errorf(format, err)),
+	}
+}
+
+func (r Result[T]) IsOk() bool {
+	return !r.IsErr()
+}
+
+func (r Result[T]) IsErr() bool {
+	return r.err.IsSome()
+}
+
+func (r Result[T]) Value() Option[T] {
+	return r.value
+}
+
+func (r Result[T]) Err() Option[error] {
+	return r.err
+}
+
+func (r Result[T]) Expect(panicMsg string) T {
+	return r.Value().Expect(panicMsg)
+}
+
+func (r Result[T]) Unwrap() T {
+	return r.Expect("value was not present in result")
+}
+
+func (r Result[T]) UnwrapOr(defaultValue T) T {
+	return Match(r, Id[T], Return[error](defaultValue))
+}
+
+func (r Result[T]) UnwrapOrElse(fn func(error) T) T {
+	return Match(r, Id[T], fn)
+}
+
+func (r Result[T]) ExpectErr(panicMsg string) error {
+	return r.Err().Expect(panicMsg)
+}
+
+func (r Result[T]) UnwrapErr() error {
+	return r.ExpectErr("err was not present in result")
+}
+
+func (r Result[T]) UnwrapErrOr(defaultValue error) error {
+	return Match(r, Return[T](defaultValue), Id[error])
+}
+
+func (r Result[T]) WrapErr(format string) Result[T] {
+	return r.MapError(func(err error) error { return fmt.Errorf(format, err) })
+}
+
+func (r Result[T]) MapError(fn func(e error) error) Result[T] {
+	return Match(r, Ok[T], Compose(fn, Err[T]))
+}
+
+func Match[Out, T any](r Result[T], okFn func(T) Out, errFn func(error) Out) Out {
+	if r.IsOk() {
+		return okFn(r.Value().Unwrap())
+	} else {
+		return errFn(r.Err().Unwrap())
+	}
+}
+
+func Map[T, U any](r Result[T], fn func(T) U) Result[U] {
+	return Match(r, Compose(fn, Ok[U]), Err[U])
+}
+
+func MapErr[T any, E error](fn func(error) E, r Result[T]) Result[T] {
+	return Match(r, Ok[T], Compose(fn, errGeneric[T, E]))
+}
+
+func AndThen[T, U any](r Result[T], fn func(T) Result[U]) Result[U] {
+	return Match(r, fn, Err[U])
+}
+
+func Map2[T, U, V any](r Result[T], s Result[U], fn func(T, U) V) Result[V] {
+	if r.IsErr() {
+		return Err[V](r.Err().Unwrap())
+	} else if s.IsErr() {
+		return Err[V](s.Err().Unwrap())
+	} else {
+		return Ok(fn(r.Value().Unwrap(), s.Value().Unwrap()))
+	}
+}
+
+func Map3[T, U, V, W any](r Result[T], s Result[U], t Result[V], fn func(T, U, V) W) Result[W] {
+	if r.IsErr() {
+		return Err[W](r.Err().Unwrap())
+	} else if s.IsErr() {
+		return Err[W](s.Err().Unwrap())
+	} else if t.IsErr() {
+		return Err[W](t.Err().Unwrap())
+	} else {
+		return Ok(fn(r.Value().Unwrap(), s.Value().Unwrap(), t.Value().Unwrap()))
+	}
+}
+
+func maybeErrorToOption(err error) Option[error] {
+	if err != nil {
+		return Some(err)
+	} else {
+		return None[error]()
 	}
 }
 
 func NewResult[T any](value T, err error) Result[T] {
 	result := Result[T]{
-		ResultE[T, error]{
-			value: &value,
-			err:   &err,
-		},
+		value: Some(value),
+		err:   maybeErrorToOption(err),
 	}
-	result.assertState()
 
 	return result
 }
 
 func NewPtrResult[T any](value *T, err error) Result[T] {
 	result := Result[T]{
-		ResultE[T, error]{
-			value: value,
-			err:   &err,
-		},
+		value: NewOptionFromPtr(value),
+		err:   maybeErrorToOption(err),
 	}
-	result.assertState()
 
 	return result
 }
 
-func Map[T, U any](r Result[T], fn func(T) U) Result[U] {
-	r.assertState()
-
-	if r.IsErr() {
-		return Err[U](*r.err)
-	} else {
-		return Ok(fn(*r.value))
-	}
+func (r Result[T]) Results() (*T, error) {
+	return r.Value().PtrRepr(), r.UnwrapErrOr(nil)
 }
 
-func MapErr[T any, E error](fn func(error) E, r Result[T]) Result[T] {
-	r.assertState()
-
-	if r.IsOk() {
-		return Ok(*r.value)
-	} else {
-		return Err[T](fn(*r.err))
-	}
-}
-
-func AndThen[T, U any](r Result[T], fn func(T) Result[U]) Result[U] {
-	r.assertState()
-
-	if r.IsErr() {
-		return Err[U](*r.err)
-	} else {
-		return fn(*r.value)
-	}
-}
-
-func AndThenWith[T, U, V any](r Result[T], s Result[U], fn func(T, U) Result[V]) Result[V] {
-	r.assertState()
-
-	if r.IsErr() {
-		return Err[V](*r.err)
-	} else if s.IsErr() {
-		return Err[V](*s.err)
-	} else {
-		return fn(*r.value, *s.value)
-	}
-}
-
-func Map2[T, U, V any](r Result[T], s Result[U], fn func(T, U) V) Result[V] {
-	r.assertState()
-
-	if r.IsErr() {
-		return Err[V](*r.err)
-	} else if s.IsErr() {
-		return Err[V](*s.err)
-	} else {
-		return Ok(fn(*r.value, *s.value))
-	}
-}
-
-func Map3[T, U, V, W any](r Result[T], s Result[U], t Result[V], fn func(T, U, V) W) Result[W] {
-	r.assertState()
-
-	if r.IsErr() {
-		return Err[W](*r.err)
-	} else if s.IsErr() {
-		return Err[W](*s.err)
-	} else if t.IsErr() {
-		return Err[W](*t.err)
-	} else {
-		return Ok(fn(*r.value, *s.value, *t.value))
-	}
-}
-
-func Match[Out, T any](r Result[T], okFn func(T) Out, errFn func(error) Out) Out {
-	if r.IsOk() {
-		return okFn(*r.value)
-	} else {
-		return errFn(*r.err)
-	}
+func (r Result[T]) UnwrappedResults() (T, error) {
+	return r.value.Unwrap(), r.UnwrapErrOr(nil)
 }
