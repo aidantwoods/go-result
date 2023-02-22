30be9e9946f20d52113497d83736189fa0f478f6
Aidan Woods <git@public.aidanwoods.com>
2023-02-22 01:53:12 +0000

    Also allow map chaining

--- apply.go
+++ apply.go
@@ -14,6 +14,10 @@ func (a ApplyToResult[O, T]) AndThen(fn func(T) Result[O]) Result[O] {
 	return AndThen(a.result, fn)
 }
 
+func (a ApplyToResult[O, T]) Map(fn func(T) O) Result[O] {
+	return Map(a.result, fn)
+}
+
 type ApplyToResult2[O1, O2, T any] struct {
 	result Result[T]
 }
@@ -26,6 +30,10 @@ func Out2[Out2, Out1, T any](result Result[T]) ApplyToResult2[Out1, Out2, T] {
 
 func (a ApplyToResult2[O1, O2, T]) AndThen(fn func(T) Result[O1]) ApplyToResult[O2, O1] {
 	return Out[O2](AndThen(a.result, fn))
+}
+
+func (a ApplyToResult2[O1, O2, T]) Map(fn func(T) O1) ApplyToResult[O2, O1] {
+	return Out[O2](Map(a.result, fn))
 }
 
 type ApplyToResult3[O1, O2, O3, T any] struct {
@@ -42,6 +50,10 @@ func (a ApplyToResult3[O1, O2, O3, T]) AndThen(fn func(T) Result[O1]) ApplyToRes
 	return Out2[O2, O3](AndThen(a.result, fn))
 }
 
+func (a ApplyToResult3[O1, O2, O3, T]) Map(fn func(T) O1) ApplyToResult2[O3, O2, O1] {
+	return Out2[O2, O3](Map(a.result, fn))
+}
+
 type ApplyToResult4[O1, O2, O3, O4, T any] struct {
 	result Result[T]
 }
@@ -56,6 +68,10 @@ func (a ApplyToResult4[O1, O2, O3, O4, T]) AndThen(fn func(T) Result[O1]) ApplyT
 	return Out3[O2, O3, O4](AndThen(a.result, fn))
 }
 
+func (a ApplyToResult4[O1, O2, O3, O4, T]) Map(fn func(T) O1) ApplyToResult3[O4, O3, O2, O1] {
+	return Out3[O2, O3, O4](Map(a.result, fn))
+}
+
 type ApplyToResult5[O1, O2, O3, O4, O5, T any] struct {
 	result Result[T]
 }
@@ -68,4 +84,8 @@ func Out5[Out5, Out4, Out3, Out2, Out1, T any](result Result[T]) ApplyToResult5[
 
 func (a ApplyToResult5[O1, O2, O3, O4, O5, T]) AndThen(fn func(T) Result[O1]) ApplyToResult4[O5, O4, O3, O2, O1] {
 	return Out4[O2, O3, O4, O5](AndThen(a.result, fn))
+}
+
+func (a ApplyToResult5[O1, O2, O3, O4, O5, T]) Map(fn func(T) O1) ApplyToResult4[O5, O4, O3, O2, O1] {
+	return Out4[O2, O3, O4, O5](Map(a.result, fn))
 }
